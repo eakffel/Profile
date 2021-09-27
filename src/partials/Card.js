8c6112b17fbb6b52b287f571d315728ce0c2600e
eakffel <60756427+eakffel@users.noreply.github.com>
2021-09-27 11:30:37 +0800

    save

--- src/partials/Card.js
+++ src/partials/Card.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import profile from '../images/profile.png'
+import Profilepic from '../images/Profilepic.jpg'
 import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import {  CgMail} from 'react-icons/cg';
@@ -9,14 +9,14 @@ function Card() {
     <div className="w-full">
       <div className="flex flex-col justify-left max-w-xs mx-auto bg-white shadow-xl rounded-xl p-5">
         <div className="">
-          <img className="w-50 mx-auto shadow-xl rounded-full" src={profile} alt="Profile face" />
+          <img className="w-45 mx-auto shadow-xl rounded-full" src={Profilepic} alt="Profile face" />
         </div>
         <div className="text-center mt-5">
           <p className="text-xl sm:text-2xl font-semibold text-gray-900">
-           衛莎莎｜卫莎莎
+          Eliza Keffeler
           </p>
           <p className="text-xl sm:text-2xl font-semibold text-gray-900">
-           Eliza Keffeler
+          衛莎莎｜卫莎莎
           </p>
           <p className="text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block border-b-2">
         
@@ -26,14 +26,15 @@ function Card() {
               <CgMail />
               <span class="sr-only">Gmail</span>
             </a>
-            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/eakffel">
-              <FaGithub />
-              <span class="sr-only">Github</span>
-            </a>
             <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="https://www.linkedin.com/in/eliza-keffeler/">
               <FaLinkedin />
               <span class="sr-only">LinkedIn</span>
               </a>
+            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/eakffel">
+              <FaGithub />
+              <span class="sr-only">Github</span>
+            </a>
+           
           </div>
         </div>
         </div>
--- src/partials/Work.js
+++ src/partials/Work.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import PDF from '../images/PDF.png'
+import MPO from '../images/MPO.jpg'
+import Post from '../images/Post.jpg'
 
 function About() {
   return (
@@ -7,17 +8,18 @@ function About() {
       <p className="text-2xl md:text-4xl font-bold text-center">
         Work Collection | 作品集
       </p>
-     <div>
+      <p className="text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto border-b-2"> Click on image to go the link ｜ 點及照片去到對應連接:</p>
+      <div className="flex align-center justify-center mt-4 ">
      <a ClassName="MPO-SM" href= "https://documentcloud.adobe.com/link/review?uri=urn:aaid:scds:US:1f8a3c84-3e4d-4c60-9e19-3517f83ceb52"target="_blank" rel="noopener noreferrer">
-     <img className="w-20 mx-auto shadow-xl rounded-full" src={PDF} alt='PDF'/>
-     <p className="text-xl font-semibold mt-4">
-      Digital Media analysis </p>
-     </a>
-
+     <img className="w-20 mx-auto sm:m-2 shadow-xl square" src={MPO} alt='PDF'/>
+</a>
+<a ClassName="POST" href= "https://documentcloud.adobe.com/link/review?uri=urn:aaid:scds:US:75e48e20-a30c-4661-bd17-100a21d6b3ac"target="_blank" rel="noopener noreferrer">
+     <img className="w-20 mx-auto sm:m-2 shadow-xl square" src={Post} alt='PDF'/>
+</a>
     </div>
       </div>
-  );
-
+      
+  )
   
 }
 
