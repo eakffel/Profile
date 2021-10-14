f6e440d474991d86dc0b3042e131fb76ce2f6403
eakffel <60756427+eakffel@users.noreply.github.com>
2021-10-14 23:15:33 +0800

    d

--- src/partials/Card.js
+++ src/partials/Card.js
@@ -7,23 +7,23 @@ import {  CgMail} from 'react-icons/cg';
 function Card() {
 
   return (
-    <div className="flex flex-col-wrap align-left w-full">
-      <div className="flex flex-col align-left justify-left max-w-xs mx-auto bg-white shadow-xl rounded-xl p-3">
+    <div className="inline-block flex flex-col-wrap align-center w-full m-5">
+      <div className="flex flex-col align-center justify-center max-w-xs mx-auto bg-white shadow-xl rounded-xl px-10 ml-8 ">
+      <div className="flex flex-col align-center justify-center max-w-xs mx-auto bg-white shadow-xl rounded-xl px-10 mr-8">
         <div className="">
-          <img className="flex flex-col sm:flex-row w-50 h-100 align-left
+          <img className="flex flex-col sm:flex-row w-40 h-100 m-3 align-center
           mx-auto shadow-xl rounded-full" src={Profilepic} alt="Profile face" />
         </div>
-        <div className="text-center mt-6">
-          <p className="text-xl sm:text-5xl font-semibold text-gray-900">
+        <div className="text-center mt-1">
+          <p className="text-xl sm:text-2xl font-semibold text-gray-700">
           Eliza Keffeler
           </p>
-          <p className="text-xl sm:text-2xl font-semibold text-gray-900">
+          <p className="text-xl sm:text-1xl font-semibold text-gray-700">
           衛莎莎｜卫莎莎
           </p>
-          <p className="text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block border-b-2">
-        
+          <p className="text-xs sm:text-base text-gray-600 pt-2 pb-1 px-20 w-auto inline-block border-b-4">
           </p>
-          <div className="flex align-center justify-center mt-4">
+          <div className="flex align-center justify-center mt-2">
           <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="mailto:eakeffel@gmail.com" target='blank'>
               <CgMail />
               <span class="sr-only">Gmail</span>
@@ -37,6 +37,7 @@ function Card() {
               <span class="sr-only">Github</span>
             </a>
            
+    </div>
     </div>
     </div>
     </div>
--- src/partials/Intro.js
+++ src/partials/Intro.js
@@ -5,8 +5,9 @@ import React from 'react'
 function Intro() {
 
     return(
-        <div className="flex flex-col-wrap align-left w-full">
-        <div className="flex flex-col align-left justify-left max-w-xs mx-auto bg-white shadow-xl rounded-xl p-3">
+        <div className="inline-block flex flex-row align-right w-full ">
+        <div className="flex align-left justify-left max-w-xs mx-auto
+         bg-white shadow-xl rounded-xl p-3 ">
           <div className="">
               <h1>Hi, my name is...
 
